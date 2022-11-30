143c1b4b73b57044f2e6aecc563821ba7b95aab0
liyuxiao222 <liyuxiao2@gmail.com>
2022-11-30 14:48:03 -0500

    a

---
+++ Apple.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Apple falls down
+ * 
+ * Liyu Xiao(your name) 
+ * (a version number or a date)
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        setLocation(getX(), getY()+1);
+    }
+}
--- elephant.java
+++ elephant.java
@@ -6,14 +6,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @Liyu Xiao 
  * @11/30/2022
  */
-public class elephant extends Actor
+public class Elephant extends Actor
 {
+    private String name;
     /**
      * Act - do whatever the elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    public Elephant (String theName){
+        name = theName;
+    }
     public void act()
     {
-        
+        move(1);
     }
+    
+    
 }
